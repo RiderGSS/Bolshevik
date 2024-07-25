94fb1aa75bb249c6c89ea6808e08a6589216ffab
RiderGSS <156878898+RiderGSS@users.noreply.github.com>
2024-07-25 15:07:03 +0700

    Установка Tailwind

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,14 +32,27 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp  # :letter_opener_web
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            '587',
+    domain:          'mail.ru',
+    user_name:       'sergav1976@gmail.com',
+    #password:       '99pMnvk0pdgki4mnFUnL',
+    password:	'vbho vxad kmrc pcny',
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
